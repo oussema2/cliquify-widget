8eb5efcf25eb3ac3eb2faedbe48db887bec139c9
Oussema Dabboussi <oussema.dabboussi99@gmail.com>
2024-11-01 22:31:38 +0100

    adding features

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,10 @@
-module.exports = function override(config, env) {
-  return {
-    ...config,
-    output: {
-      filename: "static/js/my-widget.js",
-      chunkFilename: "static/js/my-widget.[name].js",
-      path: "D:\\Work\\react-demos\\embeddable-widget\\my-embeddable-widget\\build",
-      pathinfo: false,
-      futureEmitAssets: true,
-      publicPath: "/",
-      jsonpFunction: "webpackJsonpmy-embeddable-widget",
-      globalObject: "this",
-    },
-  };
-};
+const { override } = require("customize-cra");
+
+module.exports = override((config) => {
+  if (config.output) {
+    // Set a fixed filename for the main JS file
+    config.output.filename = "static/js/main-widget.js";
+    config.output.chunkFilename = "static/js/[name].chunk.js";
+  }
+  return config;
+});
