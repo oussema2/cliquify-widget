a8f76ba78bc1de1709ad6a0dcdd2f1d8566183ad
Oussema Dabboussi <oussema.dabboussi99@gmail.com>
2024-11-01 22:25:30 +0100

    adding features

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const { override } = require("customize-cra");
+
+module.exports = override((config) => {
+  if (config.output) {
+    // Set a fixed filename for the main JS file
+    config.output.filename = "static/js/main-widget.js";
+    config.output.chunkFilename = "static/js/[name].chunk.js";
+  }
+  return config;
+});
