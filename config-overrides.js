e313a6fe03394419dc274b6a57c1566cb0813e29
Oussema Dabboussi <oussema.dabboussi99@gmail.com>
2024-11-11 00:42:56 +0100

    add test api 5

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,22 @@
 const { override } = require("customize-cra");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = override((config) => {
   if (config.output) {
-    console.log("=================================+> entereed");
     // Set a fixed filename for the main JS file
     config.output.filename = "static/js/my-widget.js";
     config.output.chunkFilename = "static/js/my-widget.[name].js";
     config.output.pathinfo = false;
     config.output.globalObject = "this";
   }
+  config.plugins = config.plugins.map((plugin) => {
+    if (plugin instanceof MiniCssExtractPlugin) {
+      return new MiniCssExtractPlugin({
+        filename: "static/css/my-widget.css", // Set fixed filename for main CSS
+        chunkFilename: "static/css/my-widget.[name].css",
+      });
+    }
+    return plugin;
+  });
   return config;
 });
